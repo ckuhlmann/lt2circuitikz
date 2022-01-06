c42e5ab35d562114ccc8d705aaffe5f169ebde78
Thomas Herpoel <thomas.herpoel@gmail.com>
2022-01-06 02:19:52 +0100

    fixing input file encoding error

--- lt2ti.py
+++ lt2ti.py
@@ -485,7 +485,7 @@ class lt2circuiTikz:
         
         self.linecnt = 0;
         try :
-            fhs = open(fileandpath, mode='r', newline=None);
+            fhs = open(fileandpath, mode='r', newline=None, encoding='iso-8859-1');
         except Exception as e:
             print('could not open ASC file "'+fileandpath+'" (cwd="'+os.curdir+'")');
             return None;
